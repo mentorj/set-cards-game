fff2dfcd583ed55ee0799afd4d804421e80db862
deadbrain <jerome@javaxpert.com>
2024-06-25 18:40:13 +0200

    REFACTORING for one method
    rewrite allTheSameOrAllDifferent in a functional way

--- src/main/java/katas/set_cards_game/SetCardsEngine.java
+++ src/main/java/katas/set_cards_game/SetCardsEngine.java
@@ -2,7 +2,10 @@ package katas.set_cards_game;
 
 
 import io.vavr.Function1;
+import io.vavr.Function2;
 import io.vavr.collection.List;
+
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class SetCardsEngine {
@@ -21,12 +24,11 @@ public class SetCardsEngine {
     private static <T> Predicate<T> asPredicate(Function1<T, Boolean> function) {
         return function::apply;
     }
+
     public boolean allTheSameOrAllDifferent(List<Card> deck){
         boolean result=true;
         List<Function1<Card,Distinguishable>> allCriterias = List.of(Card::color,Card::numberShapes,Card::shape,Card::shading);
-        for(Function1<Card,Distinguishable> function : allCriterias){
-            result=result && allTheSameOrAllDifferentByCriteria(deck,function);
-        }
-        return result;
+        var allResults = allCriterias.map(fn -> deck.groupBy(fn).size()!=2);
+        return allResults.foldLeft(true, (aBoolean, aBoolean2) -> aBoolean.booleanValue() && aBoolean2.booleanValue());
     }
 }
